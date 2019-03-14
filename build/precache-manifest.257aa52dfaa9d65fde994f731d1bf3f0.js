634f78c5756b278abf8b62258f5b4129737cbb6c
AlexCoder1996 <dima.boy.alex@gmail.com>
2019-03-14 06:41:43 +0200

    test react commit

---
+++ build/precache-manifest.257aa52dfaa9d65fde994f731d1bf3f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8721de293c2072a389ef",
+    "url": "/static/js/main.14dcb66e.chunk.js"
+  },
+  {
+    "revision": "0bd08c5f063516b7ff94",
+    "url": "/static/js/2.0c9cd3e4.chunk.js"
+  },
+  {
+    "revision": "8721de293c2072a389ef",
+    "url": "/static/css/main.41d68c1e.chunk.css"
+  },
+  {
+    "revision": "5c6a3d69dec07c0ba00b02b2c713df6b",
+    "url": "/index.html"
+  }
+];
